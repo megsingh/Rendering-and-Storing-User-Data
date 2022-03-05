845839216857fa3fc82d73d45e46cf3434965d60
meghna singh <megsingh2212@gmail.com>
2022-03-05 11:49:57 +0530

    used mongoose to for database storage

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var path = require('path');
 const bodyParser = require('body-parser');
-const {MongoClient} = require('mongodb');
+// const {MongoClient} = require('mongodb');
+const mongoose = require('mongoose');
+const client_data = require('./model/client');
+
+mongoose.connect('mongodb://localhost:27017/form_data')
 
 
 var app = express()
@@ -12,45 +16,56 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.set("views","./views")
 app.set("view engine","ejs")
 
-var url = 'mongodb://localhost:27017';
-var client  = new MongoClient(url)
+// var url = 'mongodb://localhost:27017';
+// var client  = new MongoClient(url)
 
 
-app.listen(3000,()=>{
-    console.log(`listening on port ${3000}`);
-})
+
 
 // var dbConn = client.connect();
 // var dbConn = mongodb.MongoClient.connect('mongodb://localhost:27017');
-const dbName = 'myProject';
+// const dbName = 'myProject';
 
-async function main() {
+// async function main() {
   // Use connect method to connect to the server
-  await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('feedbacks');
+  // await client.connect();
+  // console.log('Connected successfully to server');
+  // const db = client.db(dbName);
+  // const collection = db.collection('feedbacks');
 
-  app.post('/post-feedback',(req,res)=>{
-      collection.insertOne(req.body)
+  app.post('/post-feedback', async (req,res)=>{
+      const data = await client_data.create({
+      name: req.body.name,
+      email: req.body.email,
+      comment: req.body.comment
+    })
+
+    console.log(data);
+      // collection.insertOne(req.body)
       //.then(
       //res.send('data received:\n' + JSON.stringify(req.body)))
 })
 
-  app.get('/view-feedbacks',(req,res)=>{
-    collection.find({}).toArray().then((entries)=>{
-        console.log(entries);
-        res.render("table.ejs",{entries})
+  app.get('/view-feedbacks',async (req,res)=>{
+    const data = await client_data.find()
+    console.log(data);
+    res.render("table.ejs",{data})
+    // collection.find({}).toArray().then((entries)=>{
+    //     console.log(entries);
+    //     res.render("table.ejs",{entries})
+    // })
     })
-    
-            })
 
-  return 'done.';
-}
+//   return 'done.';
+// }
 
-main()
-  .then(console.log)
-  .catch(console.error)
+// main()
+//   .then(console.log)
+//   .catch(console.error)
+
+app.listen(3000,()=>{
+  console.log(`listening on port ${3000}`);
+})
 
 
 
